27af2174ef306de28c4c11b8c94ba3a52103df29
Dmitry <46465945+knaus94@users.noreply.github.com>
2024-05-22 16:07:45 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import { execute } from '@libs/core/common';
+
 enum NamingEnum {
    onLogIn = 'BettingOnLogIn',
    onRegister = 'BettingOnRegister',
@@ -6,10 +8,10 @@ enum NamingEnum {
    onRouteChange = 'BettingOnRouteChange',
 }
 
-const execute = <T>(fn: () => T): T => fn();
-
 export class BettingContent {
    private debug: boolean;
+
+   public injected = false;
 
    public styles: string;
    public scriptSrc: string;
@@ -18,26 +20,52 @@ export class BettingContent {
    public target = 'bettech';
 
    private BTRenderer: any;
-   private styleElement: HTMLStyleElement;
+   //  private styleElement: HTMLStyleElement;
+   private initialElement: HTMLMetaElement;
    private scriptElement: HTMLScriptElement;
 
    constructor(
-      base64: string,
-      config?: {
+      htmlContent: string,
+      options?: {
          target?: string;
+         themeName?: string;
+         minFrameHeight?: number;
+         betSlipOffsetTop?: number;
+         betslipZIndex?: number;
+         lang?: string;
          debug?: boolean;
          onLogin?: () => void;
          onRegister?: () => void;
          onSessionRefresh?: () => void;
          onRecharge?: () => void;
          onRouteChange?: (...params: any) => void;
+         inject?: boolean;
       },
    ) {
-      this.debug = config?.debug ?? false;
-      let htmlContent = atob(base64);
+      this.debug = options?.debug ?? false;
 
-      if (config?.target) {
-         this.target = config.target;
+      if (options?.target) {
+         this.target = options.target;
+      }
+
+      if (options?.themeName) {
+         htmlContent = htmlContent.replace(/(themeName:\s*).*?(,|\s*})/, `$1"${options.themeName}"$2`);
+      }
+
+      if (options?.minFrameHeight) {
+         htmlContent = htmlContent.replace(/(minFrameHeight:\s*).*?(,|\s*})/, `$1${options.minFrameHeight}$2`);
+      }
+
+      if (options?.betslipZIndex) {
+         htmlContent = htmlContent.replace(/(betslipZIndex:\s*).*?(,|\s*})/, `$1${options.betslipZIndex}$2`);
+      }
+
+      if (options?.betSlipOffsetTop) {
+         htmlContent = htmlContent.replace(/(betSlipOffsetTop:\s*).*?(,|\s*})/, `$1${options.betSlipOffsetTop}$2`);
+      }
+
+      if (options?.lang) {
+         htmlContent = htmlContent.replace(/(lang:\s*).*?(,|\s*})/, `$1"${options.lang}"$2`);
       }
 
       htmlContent = htmlContent.replace(/(target:\s*).*?(,|\s*})/, `$1${this.target}$2`);
@@ -48,40 +76,40 @@ export class BettingContent {
       const scriptSrcMatch = htmlContent.match(/<script src="([^"]+)"><\/script>/);
       this.scriptSrc = scriptSrcMatch ? scriptSrcMatch[1] : '';
 
-      if (config?.onLogin !== undefined) {
-         this.setWindow(NamingEnum.onLogIn, config.onLogin);
+      if (options?.onLogin !== undefined) {
+         this.setWindow(NamingEnum.onLogIn, options.onLogin);
          htmlContent = htmlContent.replace(
             /\/\*!-template-on-login--\*\/[\s\S]*?\/\*--template-on-login-!\*\//g,
-            `console.log(window); window.${NamingEnum.onLogIn}();`,
+            `window.${NamingEnum.onLogIn}();`,
          );
       }
 
-      if (config?.onRegister !== undefined) {
-         this.setWindow(NamingEnum.onRegister, config.onRegister);
+      if (options?.onRegister !== undefined) {
+         this.setWindow(NamingEnum.onRegister, options.onRegister);
          htmlContent = htmlContent.replace(
             /\/\*!-template-on-register--\*\/[\s\S]*?\/\*--template-on-register-!\*\//g,
             `window.${NamingEnum.onRegister}();`,
          );
       }
 
-      if (config?.onSessionRefresh !== undefined) {
-         this.setWindow(NamingEnum.onSessionRefresh, config.onSessionRefresh);
+      if (options?.onSessionRefresh !== undefined) {
+         this.setWindow(NamingEnum.onSessionRefresh, options.onSessionRefresh);
          htmlContent = htmlContent.replace(
             /\/\*!template-on-session-refresh--\*\/[\s\S]*?\/\*--template-on-session-refresh-!\*\//g,
             `window.betting.${NamingEnum.onSessionRefresh}();`,
          );
       }
 
-      if (config?.onRecharge !== undefined) {
-         this.setWindow(NamingEnum.onRecharge, config.onRecharge);
+      if (options?.onRecharge !== undefined) {
+         this.setWindow(NamingEnum.onRecharge, options.onRecharge);
          htmlContent = htmlContent.replace(
             /\/\*!-template-on-recharge--\*\/[\s\S]*?\/\*--template-on-recharge-!\*\//g,
             `window.${NamingEnum.onRecharge}();`,
          );
       }
 
-      if (config?.onRouteChange !== undefined) {
-         this.setWindow(NamingEnum.onRouteChange, config.onRouteChange);
+      if (options?.onRouteChange !== undefined) {
+         this.setWindow(NamingEnum.onRouteChange, options.onRouteChange);
          htmlContent = htmlContent.replace(
             /\/\*!-template-on-route-change--\*\/[\s\S]*?\/\*--template-on-route-change-!\*\//g,
             `window.${NamingEnum.onRouteChange}();`,
@@ -111,12 +139,27 @@ export class BettingContent {
       });
 
       this.debug && console.log(`Initialized params: ${this.initializeParams}`);
+
+      if (options?.inject) {
+         this.inject();
+      }
    }
 
    public inject = () => {
-      this.styleElement = document.createElement('style');
-      this.styleElement.textContent = this.styles;
-      document.head.appendChild(this.styleElement);
+      if (this.injected) {
+         return;
+      }
+
+      this.injected = true;
+
+      // this.styleElement = document.createElement('style');
+      // this.styleElement.textContent = this.styles;
+      // document.head.appendChild(this.styleElement);
+
+      this.initialElement = document.createElement('meta');
+      this.initialElement.name = 'betting-marker';
+      this.initialElement.content = 'initial';
+      document.head.appendChild(this.initialElement);
 
       this.scriptElement = document.createElement('script');
       this.scriptElement.src = this.scriptSrc;
@@ -134,19 +177,8 @@ export class BettingContent {
    };
 
    public cleanup = () => {
-      if (this.styleElement && document.head.contains(this.styleElement)) {
-         document.head.removeChild(this.styleElement);
-      }
-
-      setTimeout(() => {
-         if (this.scriptElement && document.body.contains(this.scriptElement)) {
-            document.body.removeChild(this.scriptElement);
-         }
-      }, 0);
-
-      const div = document.getElementById(this.target);
-      if (div) {
-         div.innerHTML = '';
+      if (!this.injected) {
+         return;
       }
 
       if (this.BTRenderer && typeof this.BTRenderer.kill === 'function') {
@@ -155,10 +187,33 @@ export class BettingContent {
          } catch {}
       }
 
-      if (this.styleElement) {
-         while (this.styleElement.nextSibling) {
-            document.head.removeChild(this.styleElement.nextSibling);
+      const div = document.getElementById(this.target);
+      if (div) {
+         div.innerHTML = '';
+      }
+
+      // if (this.styleElement) {
+      //    while (this.styleElement.nextSibling) {
+      //       document.head.removeChild(this.styleElement.nextSibling);
+      //    }
+      // }
+
+      // if (this.styleElement && document.head.contains(this.styleElement)) {
+      //    document.head.removeChild(this.styleElement);
+      // }
+
+      if (this.initialElement) {
+         while (this.initialElement.nextSibling) {
+            document.head.removeChild(this.initialElement.nextSibling);
          }
+      }
+
+      if (this.initialElement && document.head.contains(this.initialElement)) {
+         document.head.removeChild(this.initialElement);
+      }
+
+      if (this.scriptElement && document.body.contains(this.scriptElement)) {
+         document.body.removeChild(this.scriptElement);
       }
 
       Object.values(NamingEnum).forEach((name) => {
@@ -167,6 +222,14 @@ export class BettingContent {
          }
          this.debug && console.log('Deleted window object:', name);
       });
+
+      this.injected = false;
+   };
+
+   public changeLanguage = (lang: string) => {
+      if (this.injected && this.BTRenderer && typeof this.BTRenderer.updateOptions === 'function') {
+         this.BTRenderer.updateOptions({ lang });
+      }
    };
 
    private setWindow(type: NamingEnum, value: any) {
